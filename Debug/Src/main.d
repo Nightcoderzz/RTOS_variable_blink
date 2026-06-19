d99e12b9fe9e67a09fbc63f6d96c5de358e95841
Simon <sikorski.uws@gmail.com>
2026-06-19 22:41:15 +0100

    Working progress

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,4 +1,11 @@
-Src/main.o: ../Src/main.c \
+Src/main.o: ../Src/main.c ../Inc/FreeRTOS.h ../Inc/FreeRTOSConfig.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/projdefs.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/portable.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/deprecated_definitions.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/portable/GCC/ARM_CM0/portmacro.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/mpu_wrappers.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/task.h \
+ C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/list.h \
  C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c031xx.h \
  C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Core/Include/core_cm0plus.h \
  C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Core/Include/cmsis_version.h \
@@ -7,6 +14,15 @@ Src/main.o: ../Src/main.c \
  C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Core/Include/mpu_armv7.h \
  C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h \
  ../Inc/CLK_CONFIG.h
+../Inc/FreeRTOS.h:
+../Inc/FreeRTOSConfig.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/projdefs.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/portable.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/deprecated_definitions.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/portable/GCC/ARM_CM0/portmacro.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/mpu_wrappers.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/task.h:
+C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/FreeRTOS-Kernel/include/list.h:
 C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c031xx.h:
 C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Core/Include/core_cm0plus.h:
 C:/Users/sikor/Documents/CubeIDEWorkspace/RTOS_variable_blinking/STM32CubeC0/Drivers/CMSIS/Core/Include/cmsis_version.h:
--- Src/main.c
+++ Src/main.c
@@ -1,4 +1,5 @@
-
+#include "FreeRTOS.h"
+#include "task.h"
 #include <stdint.h>
 #include "stm32c031xx.h"
 #include "CLK_CONFIG.h"
@@ -20,7 +21,7 @@ void BLINK_INIT (){
 
 void blink_rtos (void *params){
 
-	for(;;;){
+	for(;;){
 		GPIOA->BSRR = LED_ON; //pa5 SET
         vTaskDelay(pdMS_TO_TICKS(500));
         GPIOA->BSRR = LED_OFF;
@@ -28,16 +29,18 @@ void blink_rtos (void *params){
 
 	}
 }
-xTaskCreate(blink_rtos, "blink", 128, NULL, 1, NULL,);
-vTaskStartScheduler();
+
 
 int main(void)
 {
-	BLINK_INIT ();
-	//SYS_CLK_CONFIG();
+
+xTaskCreate(blink_rtos, "blink", 128, NULL, 1, NULL);
+vTaskStartScheduler();
+BLINK_INIT ();
+//SYS_CLK_CONFIG();
 
 	while (1){
 
 
-
+	}
 }
